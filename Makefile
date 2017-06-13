6b281337049ae02bcc5df673509b5ee247d674bd
Kamil Trzcinski <ayufan@ayufan.eu>
2017-06-13 22:51:58 +0200

    Restructure configs, and add debian-stretch and ubuntu-zesty

--- Makefile
+++ Makefile
@@ -1,18 +1,37 @@
 export RELEASE_NAME ?= 0.1~dev
 
 all: \
-	xenial-mate-$(RELEASE_NAME)-armhf.tar.xz \
-	xenial-mate-$(RELEASE_NAME)-arm64.tar.xz \
-	xenial-i3-$(RELEASE_NAME)-arm64.tar.xz \
-	xenial-i3-$(RELEASE_NAME)-armhf.tar.xz \
-	xenial-minimal-$(RELEASE_NAME)-arm64.tar.xz \
-	xenial-minimal-$(RELEASE_NAME)-armhf.tar.xz
+	ubuntu-xenial-mate-$(RELEASE_NAME)-armhf.tar.xz \
+	ubuntu-xenial-mate-$(RELEASE_NAME)-arm64.tar.xz \
+	ubuntu-zesty-mate-$(RELEASE_NAME)-armhf.tar.xz \
+	ubuntu-zesty-mate-$(RELEASE_NAME)-arm64.tar.xz \
+	ubuntu-xenial-i3-$(RELEASE_NAME)-arm64.tar.xz \
+	ubuntu-xenial-i3-$(RELEASE_NAME)-armhf.tar.xz \
+	ubuntu-zesty-i3-$(RELEASE_NAME)-arm64.tar.xz \
+	ubuntu-zesty-i3-$(RELEASE_NAME)-armhf.tar.xz \
+	ubuntu-xenial-minimal-$(RELEASE_NAME)-arm64.tar.xz \
+	ubuntu-xenial-minimal-$(RELEASE_NAME)-armhf.tar.xz \
+	ubuntu-zesty-minimal-$(RELEASE_NAME)-arm64.tar.xz \
+	ubuntu-zesty-minimal-$(RELEASE_NAME)-armhf.tar.xz \
+	debian-stretch-minimal-$(RELEASE_NAME)-arm64.tar.xz \
+	debian-stretch-minimal-$(RELEASE_NAME)-armhf.tar.xz \
 
-%.tar.xz:
-	bash build.sh "$@" "$(shell basename "$@" -$(RELEASE_NAME)-$(BUILD_ARCH).tar.xz)" "$(BUILD_MODE)" "$(BUILD_SUITE)" "$(BUILD_ARCH)"
+%.xz: %
+	pxz -f -3 $<
+
+%.tar:
+	bash build.sh "$@" \
+		"$(shell basename "$@" -$(RELEASE_NAME)-$(BUILD_ARCH).tar)" \
+		"$(BUILD_MODE)" \
+		"$(BUILD_SUITE)" \
+		"$(BUILD_ARCH)"
 
 %-armhf.tar.xz: BUILD_ARCH=armhf
 %-arm64.tar.xz: BUILD_ARCH=arm64
 
-xenial-%.tar.xz: BUILD_SUITE=xenial
-xenial-%.tar.xz: BUILD_MODE=ubuntu
+ubuntu-%.tar.xz: BUILD_MODE=ubuntu
+ubuntu-xenial-%.tar.xz: BUILD_SUITE=xenial
+ubuntu-zesty-%.tar.xz: BUILD_SUITE=zesty
+
+debian-%.tar.xz: BUILD_MODE=debian
+debian-stretch-%.tar.xz: BUILD_SUITE=stretch
--- build.sh
+++ build.sh
@@ -17,6 +17,11 @@ ROOT_DIR="$(pwd)"
 
 set -ex
 
+if [[ ! -d "configs/$BUILD_VARIANT" ]]; then
+    echo "Invalid BUILD_VARIANT: $BUILD_VARIANT (missing configs/$BUILD_VARIANT)"
+    exit 1
+fi
+
 mkdir -p out/
 
 TEMP=$(mktemp -d $(readlink -f out)/build.XXXXX)
@@ -27,11 +32,22 @@ trap cleanup EXIT
 
 pushd $TEMP
 
-QEMU_BUILD_ARCH=$BUILD_ARCH
-if [[ "$QEMU_BUILD_ARCH" == "arm64" ]]; then
+if [[ "$BUILD_ARCH" == "arm64" ]]; then
     QEMU_BUILD_ARCH=aarch64
-elif [[ "$QEMU_BUILD_ARCH" == "armhf" ]]; then
+elif [[ "$BUILD_ARCH" == "armhf" ]]; then
     QEMU_BUILD_ARCH=arm
+else
+    echo "Unsupported BUILD_ARCH: $BUILD_ARCH."
+    exit 1
+fi
+
+if [[ "$BUILD_MODE" == "debian" ]]; then
+    ARCHIVE_AREAS="main contrib non-free"
+elif [[ "$BUILD_MODE" == "ubuntu" ]]; then
+    ARCHIVE_AREAS="main restricted universe multiverse"
+else
+    echo "Unsupported BUILD_MODE: $BUILD_MODE."
+    exit 1
 fi
 
 lb config \
@@ -39,14 +55,14 @@ lb config \
     --apt-recommends false \
     --apt-secure true \
     --architectures "$BUILD_ARCH" \
-    --archive-areas 'main restricted universe multiverse' \
+    --archive-areas "$ARCHIVE_AREAS" \
     --backports false \
     --binary-filesystem ext4 \
     --binary-images tar \
-    --bootstrap-qemu-arch $BUILD_ARCH \
+    --bootstrap-qemu-arch "$BUILD_ARCH" \
     --bootstrap-qemu-static "/usr/bin/qemu-$QEMU_BUILD_ARCH-static" \
     --chroot-filesystem none \
-    --compression xz \
+    --compression none \
     --distribution "$BUILD_SUITE" \
     --linux-flavours none \
     --linux-packages none \
@@ -54,10 +70,13 @@ lb config \
     --security true \
     --system normal
 
-cp -av $ROOT_DIR/configs/$BUILD_MODE/. config/
-cp -av $ROOT_DIR/configs/$BUILD_VARIANT/. config/
+for path in $BUILD_MODE $BUILD_MODE-$BUILD_SUITE $BUILD_VARIANT; do
+    if [[ -d "$ROOT_DIR/configs/$path" ]]; then
+        cp -av "$ROOT_DIR/configs/$path/." config/
+    fi
+done
 
 sudo lb build
-sudo chown "$(id -u)" binary-tar.tar.xz
+sudo chown "$(id -u)" binary-tar.tar
 
-mv binary-tar.tar.xz "$BUILD_OUTPUT"
+mv binary-tar.tar "$BUILD_OUTPUT"
